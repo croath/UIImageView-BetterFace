cf7f98dc573528bbd136f2903ec00d479ae53f30
Croath <croathliu@gmail.com>
2013-10-22 21:14:53 +0800

    demo xib

---
+++ bf/bf/BetterFaceClass/UIImageView+BetterFace.h
@@ -0,0 +1,15 @@
+//
+//  UIImageView+BetterFace.h
+//  bf
+//
+//  Created by croath on 13-10-22.
+//  Copyright (c) 2013年 Croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (BetterFace)
+
+- (void)setImage:(UIImage *)image;
+
+@end
---
+++ bf/bf/BetterFaceClass/UIImageView+BetterFace.m
@@ -0,0 +1,17 @@
+//
+//  UIImageView+BetterFace.m
+//  bf
+//
+//  Created by croath on 13-10-22.
+//  Copyright (c) 2013年 Croath. All rights reserved.
+//
+
+#import "UIImageView+BetterFace.h"
+
+@implementation UIImageView (BetterFace)
+
+- (void)setImage:(UIImage *)image{
+    [self setContentMode:UIViewContentModeCenter];
+}
+
+@end
